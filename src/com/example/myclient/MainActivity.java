50eaf379755f73552e9b802b7b8f9d3b2261be5f
xus <xus@dev-xusong.cytsonline.com>
2015-08-31 10:22:22 +0800

    更新测试

--- src/com/example/myclient/MainActivity.java
+++ src/com/example/myclient/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	    System.out.println("ssssssjjjj");
+	    System.out.println("rrrrrssssssjjjj");
 	    Log.v("MySysOut", "dddjjjjjdd");
 
 	}
